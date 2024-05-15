4361832c4d7f5b96d345390d5715c740b7cd2138
Leagues69 <201920074@stu.meu.edu.jo>
2024-05-15 08:32:37 +0300

    export Auth controller in router (forgot)

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const User = require('../Models/User');
+const auth = require('../Controllers/AuthenticateController')
 const passport = require('passport');
 const router = express.Router();
 
